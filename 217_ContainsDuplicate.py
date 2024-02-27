fadc7e648df8166e48c64eade155d085c8de82b5
palladme <55419454+palladme@users.noreply.github.com>
2024-02-27 13:19:24 -0800

    Update 217_ContainsDuplicate.py

--- 217_ContainsDuplicate.py
+++ 217_ContainsDuplicate.py
@@ -1,22 +1,42 @@
-# Time: O(n), Space: O(n)
-# len() is O(n) time
-# set() is O(n) space
+# Example 1:
 
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        hs = set(nums)        
-        return len(hs) != len(nums)
+# Input: nums = [1,2,3,1]
+# Output: true
+# Example 2:
 
+# Input: nums = [1,2,3,4]
+# Output: false
+# Example 3:
 
-# Time: O(n), Space: O(1)
-# for loop is O(n)
-# set() is O(1)
+# Input: nums = [1,1,1,3,3,4,3,2,4,2]
+# Output: true
 
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        hs = set()
-        for num in nums:
-            if num in hs:
-                return True
-            hs.add(num)
-        return False
+from typing import List
+
+def ContainsDuplicate(nums: List[int]) -> bool:
+    dup_set = set()
+
+    for num in nums:
+        if num in dup_set:
+            return True
+        dup_set.add(num)
+    return False
+
+# Testing
+test1_nums = [1,2,3,1]
+test1_expected = True
+test1_actual = ContainsDuplicate(test1_nums)
+
+assert test1_actual == test1_expected, f"Test 1 is expected to be {test1_expected} but is {test1_actual}"
+
+test2_nums = [1,2,3,4]
+test2_expected = False
+test2_actual = ContainsDuplicate(test2_nums)
+
+assert test2_actual == test2_expected, f"Test 2 is expected to be {test2_expected} but is {test2_actual}"
+
+test3_nums = [1,1,1,3,3,4,3,2,4,2]
+test3_expected = True
+test3_actual = ContainsDuplicate(test3_nums)
+
+assert test3_actual == test3_expected, f"Test 3 is expected to be {test3_expected} but is {test3_actual}"
