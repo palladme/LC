95b5eb6c6cedbc18dc2bb6c27dcd63d9ae83b1eb
palladme <55419454+palladme@users.noreply.github.com>
2024-02-27 13:19:09 -0800

    Update 49_GroupAnagrams.py

--- 49_GroupAnagrams.py
+++ 49_GroupAnagrams.py
@@ -1,12 +1,33 @@
-# Time: O(m * n)
-# Space: O(m+n); O(n) for dict, O(m) for the char
+# Example 1:
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hm = defaultdict(list)
-        for s in strs:
-            ch = [0] * 26
-            for c in s:
-                ch[ord(c) - ord('a')] += 1
-            hm[tuple(ch)].append(s)
-        return hm.values()
+# Example 2:
+# Input: strs = [""]
+# Output: [[""]]
+
+# Example 3:
+# Input: strs = ["a"]
+# Output: [["a"]]
+
+from typing import List
+from collections import defaultdict
+
+def GroupAnagrams(strs: List[str]) -> List[List[str]]:
+    ana_map = defaultdict(list)
+
+    for s in strs:
+        ch_arr = [0] * 26
+        for c in s:
+            ch_arr[ord(c) - ord('a')] += 1
+        ana_map[tuple(ch_arr)].append(s)
+    
+    return ana_map.values()
+
+# testing
+test1_strs = ["eat","tea","tan","ate","nat","bat"]
+test1_expected = [["bat"],["nat","tan"],["ate","eat","tea"]]
+test1_actual = GroupAnagrams(test1_strs)
+
+assert test1_strs.sort() == test1_expected.sort(), f"Test 1 is expected to output {test1_expected} but outputeed {test1_actual}"
+
