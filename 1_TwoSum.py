85d65336f9030765530d5ac1c456d9282c4617d6
palladme <55419454+palladme@users.noreply.github.com>
2024-02-26 19:21:12 -0800

    Update 1_TwoSum.py

--- 1_TwoSum.py
+++ 1_TwoSum.py
@@ -1,1 +1,13 @@
+# Time = O(n); O(n) for looping the array, O(1) for using "in" operator, O(1) for inserting values into hashmap.
+# Space = O(n) because Hashmap can get as large as the List.
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hm = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in hm:
+                return [hm.get(diff), i]
+            hm[nums[i]] = i
+        return []     
+        
