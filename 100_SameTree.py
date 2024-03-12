ff2c4a04ba08aa9e571cd80eb0c64e0d6598e34c
palladme <55419454+palladme@users.noreply.github.com>
2024-03-11 22:54:15 -0700

    Create 100_SameTree.py

---
+++ 100_SameTree.py
@@ -0,0 +1,29 @@
+# Example 1:
+# Input: p = [1,2,3], q = [1,2,3]
+# Output: true
+
+# Example 2:
+# Input: p = [1,2], q = [1,null,2]
+# Output: false
+
+# Example 3:
+# Input: p = [1,2,1], q = [1,1,2]
+# Output: false
+
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return False
+        
